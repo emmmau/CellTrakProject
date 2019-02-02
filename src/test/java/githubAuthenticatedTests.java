4fe696a294c13c9307d7ce0ceca6117fe3dbea27
Emmanuel Legrottaglie <emmanuel.legrottadllie@wwecorp.com>
2019-02-02 20:41:36 -0300

    Add Authenticated Tests

--- src/test/java/githubAuthenticatedTests.java
+++ src/test/java/githubAuthenticatedTests.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
 
 public class githubAuthenticatedTests {
+
+
+    @Test
+    public void verifyNeedAuthorization(){
+
+        given().
+                when().
+                get("https://api.github.com/user").
+                then().statusCode(401);
+    }
+
+    @Test
+    public void verifyAuthorizationWorks(){
+
+        given().
+                auth().preemptive().basic("username", "password").
+                when().
+                get("https://api.github.com/user").
+                then().statusCode(200);
+    }
 }
